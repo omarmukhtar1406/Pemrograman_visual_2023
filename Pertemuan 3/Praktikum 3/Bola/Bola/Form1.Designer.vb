b78e60ffd41e7102f6052e42e93fa67e697d408f
Omar Mukhtariansyah <103378418+omarmukhtar1406@users.noreply.github.com>
2023-04-28 15:15:47 +0700

    Add files via upload

---
+++ Pertemuan 3/Praktikum 3/Bola/Bola/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.txtLuas = New System.Windows.Forms.TextBox()
+        Me.txtVolume = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnbersihkan = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtJari = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(104, 131)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "Hitung"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'txtLuas
+        '
+        Me.txtLuas.Location = New System.Drawing.Point(261, 188)
+        Me.txtLuas.Name = "txtLuas"
+        Me.txtLuas.Size = New System.Drawing.Size(100, 20)
+        Me.txtLuas.TabIndex = 3
+        '
+        'txtVolume
+        '
+        Me.txtVolume.Location = New System.Drawing.Point(85, 188)
+        Me.txtVolume.Name = "txtVolume"
+        Me.txtVolume.Size = New System.Drawing.Size(100, 20)
+        Me.txtVolume.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.Label3.Location = New System.Drawing.Point(21, 195)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Volume"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.Label4.Location = New System.Drawing.Point(206, 195)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Luas"
+        '
+        'btnbersihkan
+        '
+        Me.btnbersihkan.Location = New System.Drawing.Point(195, 131)
+        Me.btnbersihkan.Name = "btnbersihkan"
+        Me.btnbersihkan.Size = New System.Drawing.Size(75, 23)
+        Me.btnbersihkan.TabIndex = 9
+        Me.btnbersihkan.Text = "Bersihkan"
+        Me.btnbersihkan.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(105, 68)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Jari - jari"
+        '
+        'txtJari
+        '
+        Me.txtJari.Location = New System.Drawing.Point(169, 61)
+        Me.txtJari.Name = "txtJari"
+        Me.txtJari.Size = New System.Drawing.Size(100, 20)
+        Me.txtJari.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Geometr415 Blk BT", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(96, 249)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(174, 14)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Omar Mukhtariansyah 200511098"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.ClientSize = New System.Drawing.Size(384, 311)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtJari)
+        Me.Controls.Add(Me.btnbersihkan)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtVolume)
+        Me.Controls.Add(Me.txtLuas)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Volume dan Luas Permukaan Bola"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents txtLuas As TextBox
+    Friend WithEvents txtVolume As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnbersihkan As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtJari As TextBox
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Pertemuan 3/Praktikum 3/Bola/Bola/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Function HitungVolume(jari As Integer)
+        Dim volume As Integer
+        volume = 4 / 3 * Math.PI * (jari * jari * jari)
+        Return volume
+    End Function
+    Private Function HitungLP(jari As Integer)
+        Dim luaspermukaan As Integer
+        luaspermukaan = 4 * Math.PI * (jari * jari)
+        Return luaspermukaan
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Dim jari, volume, luaspermukaan As Integer
+
+        jari = Val(txtJari.Text)
+        If txtJari.Text = "" Then
+            MsgBox("Informasi tidak lengkap", vbInformation, "BOLA")
+        Else
+            volume = HitungVolume(jari)
+            luaspermukaan = HitungLP(jari)
+
+            txtVolume.Text = Str(volume)
+            txtLuas.Text = Str(luaspermukaan)
+        End If
+
+    End Sub
+
+    Private Sub btnbersihkan_Click(sender As Object, e As EventArgs) Handles btnbersihkan.Click
+        txtJari.Text = ""
+        txtLuas.Text = ""
+        txtVolume.Text = ""
+
+    End Sub
+End Class
