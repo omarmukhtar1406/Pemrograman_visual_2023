cda29d6a87e60c460698fe34397a1cb69ffcddb1
Omar Mukhtariansyah <103378418+omarmukhtar1406@users.noreply.github.com>
2023-04-28 14:54:56 +0700

    Add files via upload

---
+++ Pertemuan 3/Latihan 3/Trapesium/Trapesium/Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.txtAlas = New System.Windows.Forms.TextBox()
+        Me.txtTinggi = New System.Windows.Forms.TextBox()
+        Me.txtLuas = New System.Windows.Forms.TextBox()
+        Me.txtKeliling = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnbersihkan = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtb = New System.Windows.Forms.TextBox()
+        Me.txta = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtc = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(104, 151)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "Hitung"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'txtAlas
+        '
+        Me.txtAlas.Location = New System.Drawing.Point(261, 44)
+        Me.txtAlas.Name = "txtAlas"
+        Me.txtAlas.Size = New System.Drawing.Size(100, 20)
+        Me.txtAlas.TabIndex = 1
+        '
+        'txtTinggi
+        '
+        Me.txtTinggi.Location = New System.Drawing.Point(261, 78)
+        Me.txtTinggi.Name = "txtTinggi"
+        Me.txtTinggi.Size = New System.Drawing.Size(100, 20)
+        Me.txtTinggi.TabIndex = 2
+        '
+        'txtLuas
+        '
+        Me.txtLuas.Location = New System.Drawing.Point(79, 195)
+        Me.txtLuas.Name = "txtLuas"
+        Me.txtLuas.Size = New System.Drawing.Size(100, 20)
+        Me.txtLuas.TabIndex = 3
+        '
+        'txtKeliling
+        '
+        Me.txtKeliling.Location = New System.Drawing.Point(261, 195)
+        Me.txtKeliling.Name = "txtKeliling"
+        Me.txtKeliling.Size = New System.Drawing.Size(100, 20)
+        Me.txtKeliling.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(203, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Sisi d"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(203, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Tinggi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.Label3.Location = New System.Drawing.Point(21, 202)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Luas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.Label4.Location = New System.Drawing.Point(203, 202)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Keliling"
+        '
+        'btnbersihkan
+        '
+        Me.btnbersihkan.Location = New System.Drawing.Point(195, 151)
+        Me.btnbersihkan.Name = "btnbersihkan"
+        Me.btnbersihkan.Size = New System.Drawing.Size(75, 23)
+        Me.btnbersihkan.TabIndex = 9
+        Me.btnbersihkan.Text = "Bersihkan"
+        Me.btnbersihkan.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 85)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Sisi b"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(21, 51)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(32, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Sisi a"
+        '
+        'txtb
+        '
+        Me.txtb.Location = New System.Drawing.Point(79, 78)
+        Me.txtb.Name = "txtb"
+        Me.txtb.Size = New System.Drawing.Size(100, 20)
+        Me.txtb.TabIndex = 11
+        '
+        'txta
+        '
+        Me.txta.Location = New System.Drawing.Point(79, 44)
+        Me.txta.Name = "txta"
+        Me.txta.Size = New System.Drawing.Size(100, 20)
+        Me.txta.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(21, 119)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Sisi c"
+        '
+        'txtc
+        '
+        Me.txtc.Location = New System.Drawing.Point(79, 112)
+        Me.txtc.Name = "txtc"
+        Me.txtc.Size = New System.Drawing.Size(100, 20)
+        Me.txtc.TabIndex = 14
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Geometr415 Blk BT", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(96, 256)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(174, 14)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "Omar Mukhtariansyah 200511098"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.ClientSize = New System.Drawing.Size(384, 311)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtc)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtb)
+        Me.Controls.Add(Me.txta)
+        Me.Controls.Add(Me.btnbersihkan)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtKeliling)
+        Me.Controls.Add(Me.txtLuas)
+        Me.Controls.Add(Me.txtTinggi)
+        Me.Controls.Add(Me.txtAlas)
+        Me.Controls.Add(Me.btn1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Luas dan Keliling Trapesium"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents txtAlas As TextBox
+    Friend WithEvents txtTinggi As TextBox
+    Friend WithEvents txtLuas As TextBox
+    Friend WithEvents txtKeliling As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnbersihkan As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtb As TextBox
+    Friend WithEvents txta As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtc As TextBox
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Pertemuan 3/Latihan 3/Trapesium/Trapesium/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Dim sd, tinggi, sa, sb, sc, luas, keliling As Integer
+
+        sd = Val(txtAlas.Text)
+        tinggi = Val(txtTinggi.Text)
+        sa = Val(txta.Text)
+        sb = Val(txtb.Text)
+        sc = Val(txtc.Text)
+        If txtAlas.Text = "" Or txtTinggi.Text = "" Or txta.Text = "" Or txtb.Text = "" Or txtc.Text = "" Then
+            MsgBox("Informasi tidak lengkap", vbInformation, "TRAPESIUM")
+        Else
+            luas = ((sa + sb) * tinggi) / 2
+            keliling = sa + sb + sc + sd
+
+            txtLuas.Text = Str(luas)
+            txtKeliling.Text = Str(keliling)
+        End If
+
+    End Sub
+
+    Private Sub btnbersihkan_Click(sender As Object, e As EventArgs) Handles btnbersihkan.Click
+        txtAlas.Text = ""
+        txtTinggi.Text = ""
+        txta.Text = ""
+        txtb.Text = ""
+        txtc.Text = ""
+        txtLuas.Text = ""
+        txtKeliling.Text = ""
+
+    End Sub
+End Class
