5b1c942f4568114148addf4887c9adec9bf270f6
Omar Mukhtariansyah <103378418+omarmukhtar1406@users.noreply.github.com>
2023-07-23 17:07:59 +0700

    Add files via upload

---
+++ Pertemuan 6/Praktikum/ConvertFahrenheit/ConvertFahrenheit/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub btnhtg_Click(sender As Object, e As EventArgs) Handles btnhtg.Click
+        Dim F, C, R, K As Decimal
+        F = CDec(txtFahrenheit.Text)
+        C = ToCelcius(F)
+        R = ToReamur(F)
+        K = ToKelvin(F)
+
+        txtCelcius.Text = Str(C)
+        txtReamur.Text = Str(R)
+        txtKelvin.Text = Str(K)
+    End Sub
+
+    Private Function ToCelcius(suhu As Decimal) As Decimal
+        Dim C As Decimal
+        C = 5 / 9 * (suhu - 32)
+        Return C
+    End Function
+
+    Private Function ToReamur(suhu As Decimal) As Decimal
+        Dim R As Decimal
+        R = 4 / 9 * (suhu - 32)
+        Return R
+    End Function
+
+    Private Function ToKelvin(suhu As Decimal) As Decimal
+        Dim K As Decimal
+        K = 5 / 9 * (suhu - 32) + 273
+        Return K
+    End Function
+
+    Private Sub btnbersihkan_Click(sender As Object, e As EventArgs) Handles btnbersihkan.Click
+        txtFahrenheit.Text = ""
+        txtCelcius.Text = ""
+        txtReamur.Text = ""
+        txtKelvin.Text = ""
+    End Sub
+End Class
